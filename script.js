d7ab64ccb1b2011aa61813b2abbf662cf1f69a58
zohaibhassan9 <zabaidafashion@gmail.com>
2025-02-20 10:35:25 +0500

    startStopwatch(), stopStopwatch(), and resetStopwatch() functions

---
+++ script.js
@@ -0,0 +1,39 @@
+let timer;
+let minutes= 0;
+let seconds = 0;
+let milliseconds = 0;
+let isRunning = false;
+
+
+
+function startStopwatch(){
+if(isRunning){
+isRunning = true;
+timer = setInterval(updateStopwatch, 10);
+}
+}
+
+
+function stopStopwatch(){
+if(isRunning){
+isRunning = false;
+clearInterval(timer);
+}
+}
+
+
+function resetStopwatch(){
+stopStopwatch();
+minutes = 0;
+seconds = 0;
+milliseconds = 0;
+updateDisplay();
+
+}
+
+
+function updateDisplay(){
+
+
+
+}
